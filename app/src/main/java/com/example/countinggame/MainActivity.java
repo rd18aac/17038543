f4745e8b89b6de2c78004fd7556809331cbf9655
Robert Dixon <robert@roberts-air.home>
2020-10-21 02:56:14 +0100

    Created finishing screen with star animations, play again button and hid overlapping elements when correct answer is chosen

--- app/src/main/java/com/example/countinggame/MainActivity.java
+++ app/src/main/java/com/example/countinggame/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.countinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -76,7 +80,74 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "NOPE", Toast.LENGTH_SHORT).show();
         }
         else if (v.getId() == R.id.button5) {
-            Toast.makeText(this, "CORRECT!!!!!", Toast.LENGTH_SHORT).show(); // Correct answer = 5
+            // Get question text element
+            TextView question = findViewById(R.id.question);
+
+            // Set question text to display correct answer and change it to green
+            question.setText("3 + 2 = 5");
+            question.setTextColor(Color.parseColor("#52b465"));
+
+            // Make number buttons, grey background, plate and apple images invisible
+            findViewById(R.id.button0).setVisibility(View.INVISIBLE);
+            findViewById(R.id.button1).setVisibility(View.INVISIBLE);
+            findViewById(R.id.button2).setVisibility(View.INVISIBLE);
+            findViewById(R.id.button3).setVisibility(View.INVISIBLE);
+            findViewById(R.id.button4).setVisibility(View.INVISIBLE);
+            findViewById(R.id.button5).setVisibility(View.INVISIBLE);
+            findViewById(R.id.button6).setVisibility(View.INVISIBLE);
+            findViewById(R.id.button7).setVisibility(View.INVISIBLE);
+            findViewById(R.id.button8).setVisibility(View.INVISIBLE);
+            findViewById(R.id.button9).setVisibility(View.INVISIBLE);
+            findViewById(R.id.background).setVisibility(View.INVISIBLE);
+            findViewById(R.id.plate).setVisibility(View.INVISIBLE);
+            findViewById(R.id.apple1).setVisibility(View.INVISIBLE);
+            findViewById(R.id.apple2).setVisibility(View.INVISIBLE);
+            findViewById(R.id.apple3).setVisibility(View.INVISIBLE);
+            findViewById(R.id.apple4).setVisibility(View.INVISIBLE);
+            findViewById(R.id.apple5).setVisibility(View.INVISIBLE);
+            findViewById(R.id.apple6).setVisibility(View.INVISIBLE);
+            findViewById(R.id.apple7).setVisibility(View.INVISIBLE);
+            findViewById(R.id.apple8).setVisibility(View.INVISIBLE);
+            findViewById(R.id.apple9).setVisibility(View.INVISIBLE);
+
+            // Make "play again" button visible
+            findViewById(R.id.againButton).setVisibility(View.VISIBLE);
+
+            // Get star image elements
+            ImageView star1 = (ImageView)findViewById(R.id.star1);
+            ImageView star2 = (ImageView)findViewById(R.id.star2);
+            ImageView star3 = (ImageView)findViewById(R.id.star3);
+            ImageView star4 = (ImageView)findViewById(R.id.star4);
+
+            // Make star images visible
+            star1.setVisibility(View.VISIBLE);
+            star2.setVisibility(View.VISIBLE);
+            star3.setVisibility(View.VISIBLE);
+            star4.setVisibility(View.VISIBLE);
+
+            // Define animations
+            AlphaAnimation anim1 = new AlphaAnimation(0, 1);
+            anim1.setDuration(2000);
+            anim1.setRepeatCount(Animation.INFINITE);
+
+            AlphaAnimation anim2 = new AlphaAnimation(0, 1);
+            anim2.setDuration(2500);
+            anim2.setRepeatCount(Animation.INFINITE);
+
+            AlphaAnimation anim3 = new AlphaAnimation(0, 1);
+            anim3.setDuration(1500);
+            anim3.setRepeatCount(Animation.INFINITE);
+
+            AlphaAnimation anim4 = new AlphaAnimation(0, 1);
+            anim4.setDuration(2500);
+            anim4.setRepeatCount(Animation.INFINITE);
+            anim4.setStartOffset(1500); // Animation 4 gets set off after animation 3 has loaded
+
+            // Set off animations for star images
+            star1.startAnimation(anim1);
+            star2.startAnimation(anim2);
+            star3.startAnimation(anim3);
+            star4.startAnimation(anim4);
         }
         else if (v.getId() == R.id.button6) {
             Toast.makeText(this, "NOPE", Toast.LENGTH_SHORT).show();
